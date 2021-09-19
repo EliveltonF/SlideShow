3c45f957ac1db86b1a403d755a766708cfbf9bbf
EliveltonF <diogoelivelton18@gmail.com>
2021-09-19 12:49:23 -0300

    SlideShow

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
-let num1;
-let num2;
-let num3
-let vari;
-let banco = [];
-let a;
-var veri = true
-let consta = false
+let totalSlide = 3
+let currentSlide= 0;
 
-
-
-
-
-
-function aa(event) {
-    var variavel = document.getElementById('dentroVisor').innerHTML;
-    var tamanho = variavel.toString();
-    console.log(tamanho.length,tamanho)
-    if(tamanho.length < 12) {
-        document.getElementById('dentroVisor').innerHTML = (variavel + event)
-    }else {
-        alert('tamanho excecido')
+function goPrev() {
+    currentSlide--;
+    if(currentSlide < 0) {
+        currentSlide = totalSlide - 1;
     }
+    bota();
 }
-function back() {
-    var variavel = document.getElementById('dentroVisor').innerHTML;
-    document.getElementById('dentroVisor').innerHTML = variavel.substring(0,variavel.length-1)
-}
-function igual() {
-
-    var variavel = document.getElementById('dentroVisor').innerHTML;
-    if(variavel) {
-        document.getElementById('dentroVisor').innerHTML = eval(variavel);
+function goNext() {
+    currentSlide++;
+    if(currentSlide > (totalSlide-1)) {
+        currentSlide = 0;
     }
+    bota();
 }
 
-
-
-
-
-
+function bota() {
+    console.log(document.getElementById('Img'),'img')
+    let medidaTela = 1359
+    console.log('aa')
+    console.log(totalSlide)
+    let newMargin = currentSlide * medidaTela;
+    console.log(newMargin)
+    document.getElementById('img').style.marginLeft = `-${newMargin}px`;
+}
